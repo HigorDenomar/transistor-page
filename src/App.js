f400d49ef7a34355a14da040c545c9ea41a83f24
Higor Denomar <higordenomar@hotmail.com>
2020-08-12 15:02:57 -0300

    first section created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import swordImg from './assets/sword.png';
+import heroImg from './assets/hero.png';
 
 function App() {
   return (
@@ -10,6 +11,16 @@ function App() {
         <img src={swordImg} alt="Espada"/>
         <h1>SUPERGIANTGAMES</h1>
       </header>
+
+      <main>
+        <section id="section-01">
+          <div className="contain-hero">
+            <h2>TRANSISTOR - RED THE SINGER</h2>
+            <img src={heroImg} alt="Hero" />
+            <p>"Olha, o que quer que você esteja pensando, me faça um favor, não solte."</p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
